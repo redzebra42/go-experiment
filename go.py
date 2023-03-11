5b2e79aaa3e4395c3e1b3ef40e88501d52a2da8f
Tristan Delaigue <tristan@nowhere.com>
2023-03-11 21:48:26 +0100

    Simplify hand_to_coord

--- go.py
+++ go.py
@@ -9,16 +9,13 @@ class Go():
         self.opp_player = 'b' if self.current_player == 'w' else 'w'
         
 
-    def trans_hand(self,hand):
+    def hand_to_coord(self,hand):
+        '''Convert a textual hand like c12 into coordinates like (2, 12).'''
         self.list = ['a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t']
-        for i in range(len(self.list)):
-            if len(hand) == 2 and hand[0] == self.list[i]:
-                return (i, int(hand[1])-1)
-            elif len(hand) == 3 and hand[0] == self.list[i]:
-                return (i, 10*int(hand[1]) + int(hand[2]) - 1)
+        return (self.list.index(hand[0]), int(hand[1:]))
 
     def move(self, hand):
-        self.board.goban[self.trans_hand(hand)[1]][self.trans_hand(hand)[0]] = self.current_player
+        self.board.goban[self.hand_to_coord(hand)[1]][self.hand_to_coord(hand)[0]] = self.current_player
 
     def next_turn(self,player):
         if player == 'w':
@@ -28,10 +25,10 @@ class Go():
 
     
     def neighbours(self,hand):
-        return [self.board.goban[self.trans_hand(hand)[0]][self.trans_hand(hand)[1] - 1],
-                self.board.goban[self.trans_hand(hand)[0] - 1][self.trans_hand(hand)[1]],
-                self.board.goban[self.trans_hand(hand)[0]][self.trans_hand(hand)[1] + 1],
-                self.board.goban[self.trans_hand(hand)[0] + 1][self.trans_hand(hand)[1]],]
+        return [self.board.goban[self.hand_to_coord(hand)[0]][self.hand_to_coord(hand)[1] - 1],
+                self.board.goban[self.hand_to_coord(hand)[0] - 1][self.hand_to_coord(hand)[1]],
+                self.board.goban[self.hand_to_coord(hand)[0]][self.hand_to_coord(hand)[1] + 1],
+                self.board.goban[self.hand_to_coord(hand)[0] + 1][self.hand_to_coord(hand)[1]],]
 
 
     def capture (self, hand):
