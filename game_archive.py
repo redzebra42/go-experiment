e3ea7dbab38b646a7d87e1be9a316997377f91b2
Tristan Delaigue <tristan@nowhere.com>
2023-03-12 10:38:09 +0100

    try to implement archive translation

---
+++ game_archive.py
@@ -0,0 +1,10 @@
+class Game_archive():
+
+    def __init__(self) -> None:
+        pass
+
+    def sfg_to_move_list():
+        sfg = open('game_1.sgf')
+        print(sfg[1])
+
+Game_archive.sfg_to_move_list()
