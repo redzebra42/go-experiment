2ac041d4f03064e98c6f2bf7d209268600bfc9cf
Tristan Delaigue <tristan@nowhere.com>
2023-04-09 09:42:44 +0200

    Typing moves works

--- main.py
+++ main.py
@@ -10,16 +10,6 @@ if __name__ == "__main__":
     root.geometry("1100x800")
     frm = ttk.Frame(root, padding=10)
     my_canvas = Canvas(root, width=1200, height= 800)
-    my_canvas.pack(side=LEFT)
-    my_canvas.create_rectangle(40,40,710,710, width=3, fill="orange", outline="orange")
-    my_canvas.create_rectangle(60,60,690,690, width=3)
-    for i in range(1,18):
-        my_canvas.create_line(60+35*i,60,60+35*i,690, width=3)
-    for j in range(1,18):
-        my_canvas.create_line(60,60+35*j,690,60+35*j, width=3)
-    for i in [165, 375, 585]:
-        for j in [165, 375, 585]:
-            my_canvas.create_oval(i-5,j-5,i+5,j+5, fill="black")
 
     def play():
         hand = text_box.get()
