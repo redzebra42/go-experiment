1d9ca154eb761c5d079afdd9123ee30470fe942c
Tristan Delaigue <tristan@nowhere.com>
2023-05-19 12:51:09 +0200

    Win and who won for ttt

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,26 +1,35 @@
 import copy
+import random
 
 class TTT():
 
-    def __init__(self, ttt_board =[["X","X","X"],
+    def __init__(self, ttt_board =[["_","_","_"],
                                    ["_","_","_"],
-                                   ["X","_","_"]] ) -> None:
+                                   ["_","_","_"]] ) -> None:
         self.ttt_board = copy.deepcopy(ttt_board)
         self.curr_player = "X"
 
     def print_ttt(self):
+        print("")
         for i in self.ttt_board:
             line = ""
             for j in i:
                 line += j
             print(line)
-        print("")
     
+    def other_player(self, player):
+        if player == "X":
+            return "O"
+        elif player == "O":
+            return "X"
+        else:
+            return "error"
+
     def next_turn(self):
         if self.curr_player == "X":
             self.curr_player = "O"
         else:
-            self.curr_player = "x"
+            self.curr_player = "X"
     
     def is_legal(self,coord):
         return self.ttt_board[coord[1]][coord[0]] == "_"
@@ -31,23 +40,39 @@ class TTT():
             self.next_turn()
     
     def has_won(self):
-        cp = self.curr_player
+        cp = self.other_player(self.curr_player)
         brd = self.ttt_board
         for i in range(len(brd)):
             if (brd[i][0], brd[i][1], brd[i][2]) == (cp, cp, cp):
-                return True
+                return (True, cp)
             elif (brd[0][i], brd[1][i], brd[2][i]) == (cp, cp, cp):
-                return True
+                return (True, cp)
         if (brd[0][0],brd[1][1],brd[2][2]) == (cp, cp, cp):
-            return True
+            return (True, cp)
         elif (brd[0][2],brd[1][1],brd[2][0]) == (cp, cp, cp):
-            return True
-        return False
-            
+            return (True, cp)
+        return (False, None)
+    
+    def legal_moves(self):
+        legal_moves = []
+        l = len(self.ttt_board)
+        for i in range(l):
+            for j in range(l):
+                coord = (i, j)
+                if self.is_legal((i, j)):
+                    legal_moves.append(coord)
+        return legal_moves
+
+    def play_random(self):
+        leg_moves = self.legal_moves()
+        i = random.randint(0, len(leg_moves))
+        self.move(leg_moves[i-1])
 
 
 if __name__ == "__main__":
     ttt = TTT()
-    ttt.print_ttt()
-    print(ttt.has_won())
+    for i in range(9):
+        ttt.play_random()
+        ttt.print_ttt()
+        print(ttt.has_won())
     
