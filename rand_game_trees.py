53915637eaf234739f56915c1306303ae4cfb79c
Tristan Delaigue <tristan.del44@gmail.com>
2023-11-13 15:42:57 +0100

    Enfants works, each floor starts at 0

--- rand_game_trees.py
+++ rand_game_trees.py
@@ -20,11 +20,11 @@ clone()
 
 class RGTstate():
 
-    def __init__(self, position=(0, 1), best_move=random.randint(1, math.factorial(global_size)), board_size=global_size) -> None:
+    def __init__(self, position=(0, 0), best_move=random.randint(1, math.factorial(global_size)), board_size=global_size) -> None:
         self.board_size = board_size
         self.best_move = best_move
         self.position = position
-        self.tree = {}
+        self.tree = {(0, 0):0}
         self.init_tree()
 
     def init_tree(self):
@@ -33,12 +33,11 @@ class RGTstate():
             self.tree[pos] = 1
 
     def update_tree(self, path):
-        #dictionnary with the values of the tree's nodes (best path values are initialized here and random values will be added when calculating a score)
         for pos in path:
-            if not (pos in self.tree.values()) :
+            if not (pos in self.tree.keys()) :
                 self.tree[pos] = random.randint(-10,10)/10
 
-    def path(self, destination):
+    def path(self, destination:int):
         nb_tot_moves = (math.factorial(self.board_size))
         frac = destination/nb_tot_moves
         path = []
@@ -49,7 +48,12 @@ class RGTstate():
                 j += 1
             path.append((i + 1, j))
         return path
-
+    
+    def enfants(self, node) -> list:
+        enf_list = []
+        for i in range(self.board_size - node[0]):
+            enf_list.append((node[0] + 1, node[1] * (self.board_size - node[0]) + i))
+        return enf_list
 
 
     def curr_player(self):
@@ -67,10 +71,7 @@ class RGT():
         self.state = state
 
     def legal_moves(self, state):
-        leg_pos = []
-        for i in range(self.state.board_size - state.position[0]):
-            leg_pos.append((state.position[0] + 1, (state.position[1] - 1) * (state.position[0] + 1) + i + 1))
-        return leg_pos
+        return state.enfants(state.position)
 
     def play_at(self, state, move):
         state.position = move
@@ -79,7 +80,8 @@ class RGT():
         return state.position[0] == state.board_size - 1
 
     def winner(self, state):
-        #TODO peut etre switch les valeurs du gagnant (ou y reflechir au moins xD) on rappelle que le score d'une case est positif si elle est bénéfique au joueur 1 (ou 0 mais faut se décider) et ce pour tout noeud.
+        #TODO peut etre switch les valeurs du gagnant (ou y reflechir au moins) on rappelle que le score 
+        #d'une case est positif si elle est bénéfique au joueur 1 (ou 0 mais faut se décider) et ce pour tout noeud.
         if self.is_over(state):
             path = state.path(state.position[1])
             state.update_tree(path)
@@ -98,6 +100,31 @@ class RGT():
 
     def play_mct(self):
         pass
+
+    def _tree_dict_to_list(self):
+        node_list = [[] for i in range(self.state.board_size)]
+        for i in range(self.state.board_size):
+            for node in self.state.tree:
+                node_list[i].append(node[1])
+        return node_list
+    
+    def _pretty_print_rec(self, node, file):
+        if node[0] != 0:
+            file.write("\n")
+            for i in range(node[0]):
+                file.write("    ")
+        else:
+            file.write(str(self.state.tree) + "\n")
+        file.write(f"({node[0]}/{node[1]}/{self.state.tree[node]})")
+        for enf in self.state.enfants(node):
+            if enf in self.state.tree:
+                self._pretty_print_rec(enf, file)
+        
+    
+    def pretty_print(self, file):
+        '''prints depth/position/value for each nodes with a value'''
+        #TODO doesn't work yet, could try a non-recursive approach, or just give up, this function doesn't really matters anyways...
+        self._pretty_print_rec((0, 0), file)
         
 if __name__ == "__main__":
     pos = (3, 5)
@@ -114,4 +141,6 @@ if __name__ == "__main__":
     print(state.best_move)
     print(state.path(state.best_move))
     print(rgt.winner(state))
-    print(state.tree)
+    print(len(state.tree))
+    print(rgt.state.enfants((1,3)))
+    rgt.pretty_print(open("RGT_tree.lsp", "w"))
