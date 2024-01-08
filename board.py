3a54a56cd383bb0245744bf872dcd42ccff82be2
Tristan Delaigue <tristan.del44@gmail.com>
2024-01-08 14:08:31 +0100

    old working update_legal_move

--- board.py
+++ board.py
@@ -245,25 +245,7 @@ class Board():
         clock = time.clock_gettime(0)
         if move != 'pass':
             self.leg_move_board[move[1]][move[0]] = []
-            if captures > 0 or self.liberty(self.group(move))[0] == 0:
-                self.leg_move_board = self.initiate_legal_moves()
-            else:
-                move_lib = self.liberty(self.group(move))
-                new_coord = move_lib[1][0]
-                if move_lib[0] == 1 and curr_player in self.leg_move_board[new_coord[1]][new_coord[0]]:
-                    self.leg_move_board[new_coord[1]][new_coord[0]].remove(curr_player)
-                for neighb in self.neighbours(move):
-                    nei_group = self.group(neighb)
-                    nei_lib = self.liberty(nei_group)
-                    if self.goban[neighb[1]][neighb[0]] == self.opposite(curr_player) and nei_lib[0] == 1:
-                        new_coord = nei_lib[1][0]
-                        if self.is_suicide(new_coord, self.opposite(curr_player)) and self.opposite(curr_player) in self.leg_move_board[new_coord[1]][new_coord[0]]:
-                            self.leg_move_board[new_coord[1]][new_coord[0]].remove(self.opposite(curr_player))
-                    elif self.goban[neighb[1]][neighb[0]] == '0':
-                        if self.is_suicide(neighb, curr_player) and curr_player in self.leg_move_board[neighb[1]][neighb[0]]:
-                            self.leg_move_board[neighb[1]][neighb[0]].remove(curr_player)
-                        elif self.is_suicide(neighb, self.opposite(curr_player)) and self.opposite(curr_player) in self.leg_move_board[neighb[1]][neighb[0]]:
-                            self.leg_move_board[neighb[1]][neighb[0]].remove(self.opposite(curr_player))
+            self.leg_move_board = self.initiate_legal_moves()
         #print("update legal moves: ", time.clock_gettime(0) - clock)
 
     def initiate_legal_moves(self):
